5f21125996297ba4b873a970b894e119a123c625
Debetiesto <luisvazquez8942@gmail.com>
2026-02-06 14:16:24 +0100

    Variable Cambio

--- src/main/java/eus/tartanga/psp/server/model/VideoItem.java
+++ src/main/java/eus/tartanga/psp/server/model/VideoItem.java
@@ -1,51 +1,63 @@
 package eus.tartanga.psp.server.model;
 
 public class VideoItem {
-	private Long id;
-	private String titulo;
-	private String descripcion;
-	private String nombreArchivo; // Ej: "trailer_gta.mp4"
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String nombreArchivo;
+    private String urlYoutube;
 
-	public VideoItem() {
-	}
+    // Constructor vacío necesario para Jackson
+    public VideoItem() {
+    }
 
-	public VideoItem(Long id, String titulo, String descripcion, String urlYoutube) {
-		this.id = id;
-		this.titulo = titulo;
-		this.descripcion = descripcion;
-		this.nombreArchivo = urlYoutube;
-	}
+    public VideoItem(Long id, String titulo, String descripcion, String nombreArchivo, String urlYoutube) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.nombreArchivo = nombreArchivo;
+        this.urlYoutube = urlYoutube;
+    }
 
-	// Getters y Setters...
-	public Long getId() {
-		return id;
-	}
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public String getDescripcion() {
-		return descripcion;
-	}
+    public String getDescripcion() {
+        return descripcion;
+    }
 
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 
-	public String getNombreArchivo() {
-		return nombreArchivo;
-	}
+    public String getNombreArchivo() {
+        return nombreArchivo;
+    }
 
-	public void setNombreArchivo(String nombreArchivo) {
-		this.nombreArchivo = nombreArchivo;
-	}
+    public void setNombreArchivo(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+
+    // --- GETTERS Y SETTERS NUEVOS ---
+    public String getUrlYoutube() {
+        return urlYoutube;
+    }
+
+    public void setUrlYoutube(String urlYoutube) {
+        this.urlYoutube = urlYoutube;
+    }
 }
